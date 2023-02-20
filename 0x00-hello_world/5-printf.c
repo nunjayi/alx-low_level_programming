293cc08260858633add356365177d05c5345cb06
nunjayi <epenyash@gmail.com>
2023-02-20 03:21:09 -0800

    using printf intad of puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>/
+/**
+*main - entrypoint
+*RETURN: Always 0 (SUCCESS)
+*/
+int main(void)
+{
+ printf("\"with proper grammar, but the outcome is a piece of art,");
+ return (0);
+}
