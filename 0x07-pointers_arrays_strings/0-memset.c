ae7ff13f0887908dd3628f2ccbb03757dced29f4
nunjayi <epenyash@gmail.com>
2023-03-06 11:39:46 -0800

    tasl 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,20 @@
-#include"main.h"
+#include "main.h"
 /**
-*_memset-fill the nbytes @s
-*@s:a pointer
-@c:fill memory are.
-*@n:bytes value to be filled
-*Return:A pointer to the filed memory area 
-*/
-void*_memset(void *s, int c, size_t n
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int index; 
-unsigned char *memory = s, value = c;
- for (index = 0; index < n; index++)
- memory[index] = value;
-return (memory);
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
